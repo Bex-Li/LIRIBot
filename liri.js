2d8e4a88db78791cadb659cf637061d60e461033
Bex-Li <rebekkikko@gmail.com>
2018-05-08 18:39:09 -0700

    updated files and added JSON packages

--- liri.js
+++ liri.js
@@ -1,26 +1,26 @@
 require("dotenv").config();
+console.log("Hello")
 
-// write twitter function 
-// write spotify function
-// write OMDB function
 var twitter = require("twitter")
 var spotify = require("node-spotify-api")
+var omdb = require("omdb-client")
 var request = require("request")
-// var keys = require("./keys.js")
 var fs = require("fs")
+var keys = require("./key")
 
 var input1 = process.argv[2]
 var input2 = process.argv[3]
 
 if (input1 === 'my-tweets') {
     //call the function here
+    callingTwitter()
     console.log("you chose my-tweets")
 } else if (input1 === "spotify-this-song") {
     //call the function here
     console.log("you chose spotify-this-song")
 } else if (input1 === "movie-this") {
     //call the function here
-    movie(input2)
+    movie(input2);
     console.log("you chose movie-this")
 } else if (input1 === "do-what-it-says") {
     //call the function here    
@@ -30,35 +30,66 @@ if (input1 === 'my-tweets') {
     console.log("please re-enter your option")
 }
 
-// function twitter () {
-//     console.log("Tweet tweet")
-// }
+function callingTwitter() {
+    console.log("Tweet tweet")
+    var client = new twitter(keys.twitter)
+    console.log("This is where the this is loaded is coming from:", client)
+    var params = {
+        q: 'Obama',
+        count: 20
+    }
+    client.get('search/tweets', params, function(error, tweets, response) {
+        if (!error) {
+          console.log(tweets);
+        }
+        else {
+            console.log(error)
+        }
+      });
+}
+    // show your last 20 tweets and when they were created at in your terminal/bash win}
+
+
 // function spotify (songName) {
-//     console.log("Nice Song!")
+//     console.log("Nice Song!"This will show the following information about the song in your terminal/bash window
+// Artist(s)
+// The song's name
+// A preview link of the song from Spotify
+// The album that the song is from
+// If no song is provided then your program will default to "The Sign" by Ace of Base.
+// You will utilize the node-spotify-api package in order to retrieve song information from the Spotify API. 
+// 
 // }
 function movie(movieName) {
     console.log("Cool Movie!")
-    var movieUrl = "https://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-    console.log(movieUrl)
-    request(movieUrl, function(err, res, body){
-        var movieObject = JSON.parse(res.body)
-        console.log(movieObject)
+    var params = {
+        apiKey: '9365097a',
+        title: movieName,
+        year: parseInt(process.argv[4])
+    }
+    if (params.title === " " || params.title === undefined) {
+        params.title = "Mr Nobody";
+        params.year = 0;
+        console.log("If you haven't watched \"Mr. Nobody,\" then you should: http://www.imdb.com/title/tt0485947/");
+        console.log("It's on Netflix!");
+    } 
+    // var movieUrl = "https://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+    console.log(params)
+    omdb.get(params, function (err, res) {
         console.log(`
-            Title: ${ movieObject.Title }
-            Year: ${ movieObject.Year }
-            imdbRating: ${ movieObject.imdbRating }
-            Rotten Tomatoes: ${ movieObject.Ratings[1].Value }
-            Country Produced: ${ movieObject.Country }
-            Language: ${ movieObject.Language }
-            Plot: ${ movieObject.Plot }
-            Actors: ${ movieObject.Actors }
+            Title: ${ res.Title}
+            Year: ${ res.Year}
+            imdbRating: ${ res.imdbRating}
+            Rotten Tomatoes: ${ res.Ratings[1].Value}
+            Country Produced: ${ res.Country}
+            Language: ${ res.Language}
+            Plot: ${ res.Plot}
+            Actors: ${ res.Actors}
         `)
     })
 }
-
-
 // function doWhat () {
-//     console.log("Liri is my bitch!")
+//     console.log("Liri is my Awesome!")
 // }
 
 //where would we call these functions? 
